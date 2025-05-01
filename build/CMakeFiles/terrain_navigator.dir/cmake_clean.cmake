708f1e507bdb511e9de3cf5b408b87bafdcc8f12
DebugDestroy <191554025+DebugDestroy@users.noreply.github.com>
2025-05-01 22:37:41 +0300

    FINAL: Корректное игнорирование build/

---
+++ build/CMakeFiles/terrain_navigator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terrain_navigator.dir/src/app/main.cpp.o"
+  "CMakeFiles/terrain_navigator.dir/src/app/main.cpp.o.d"
+  "bin/terrain_navigator"
+  "bin/terrain_navigator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/terrain_navigator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
